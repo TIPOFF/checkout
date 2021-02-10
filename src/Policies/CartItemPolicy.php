7ecf1a6d96aad046a44ea30d9216a298064c5713
Chris H <crishack8@gmail.com>
2021-02-09 23:12:53 -0600

    Add Policies

---
+++ src/Policies/CartItemPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Tipoff\Checkout\Policies;
+
+use Tipoff\Checkout\Models\CartItem;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CartItemPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\CartItem  $cartItem
+     * @return mixed
+     */
+    public function view(User $user, CartItem $cartItem)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\CartItem  $cartItem
+     * @return mixed
+     */
+    public function update(User $user, CartItem $cartItem)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\CartItem  $cartItem
+     * @return mixed
+     */
+    public function delete(User $user, CartItem $cartItem)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\CartItem  $cartItem
+     * @return mixed
+     */
+    public function restore(User $user, CartItem $cartItem)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\CartItem  $cartItem
+     * @return mixed
+     */
+    public function forceDelete(User $user, CartItem $cartItem)
+    {
+        return true;
+    }
+}
