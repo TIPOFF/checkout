b98c1d5749a3c57a70e7f773916450a823b8b1a5
Patrick Breen <patrick.breen@quartzy.com>
2021-02-11 08:10:19 -0500

    Change permission seeder to migration; prep for latest test-support

---
+++ tests/Unit/Migrations/PermissionsMigrationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Checkout\Tests\Unit\Migrations;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\PermissionServiceProvider;
+use Tipoff\Checkout\Tests\TestCase;
+
+class PermissionsMigrationTest extends TestCase
+{
+    use DatabaseTransactions;
+
+
+    protected function getPackageProviders($app)
+    {
+        return array_merge(parent::getPackageProviders($app), [
+            PermissionServiceProvider::class,
+        ]);
+    }
+
+    /** @test */
+    public function permissions_seeded()
+    {
+        $this->assertTrue(Schema::hasTable('permissions'));
+        $this->assertDatabaseCount('permissions', 1);
+    }
+}
