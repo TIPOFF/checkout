0e31462eff122df2ed03186755535a727e746a73
pdbreen <pdbreen@users.noreply.github.com>
2021-02-11 13:10:53 +0000

    Fix styling

--- tests/Unit/Migrations/PermissionsMigrationTest.php
+++ tests/Unit/Migrations/PermissionsMigrationTest.php
@@ -13,7 +13,6 @@ class PermissionsMigrationTest extends TestCase
 {
     use DatabaseTransactions;
 
-
     protected function getPackageProviders($app)
     {
         return array_merge(parent::getPackageProviders($app), [
