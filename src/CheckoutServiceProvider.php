7957e9ac9d5486f4723bda8b12344f02cdb18ab0
Drew Roberts <github@drewroberts.com>
2021-02-13 10:22:23 -0500

    Remove extra space

--- src/CheckoutServiceProvider.php
+++ src/CheckoutServiceProvider.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Tipoff\Checkout;
 
-use Tipoff\Checkout\Contracts\Models\CartInterface;	
-use Tipoff\Checkout\Contracts\Models\CartItemInterface;	
+use Tipoff\Checkout\Contracts\Models\CartInterface;
+use Tipoff\Checkout\Contracts\Models\CartItemInterface;
 use Tipoff\Checkout\Contracts\Models\OrderInterface;
 use Tipoff\Checkout\Models\Cart;
 use Tipoff\Checkout\Models\CartItem;
@@ -21,10 +21,10 @@ class CheckoutServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
-            ->hasModelInterfaces([	
-                CartInterface::class => Cart::class,	
-                CartItemInterface::class => CartItem::class,	
-                OrderInterface::class => Order::class,	
+            ->hasModelInterfaces([
+                CartInterface::class => Cart::class,
+                CartItemInterface::class => CartItem::class,
+                OrderInterface::class => Order::class,
             ]);
             ->hasPolicies([
                 Cart::class => CartPolicy::class,
