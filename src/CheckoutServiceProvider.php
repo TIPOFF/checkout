19ccd925be68b4865875a50beb1cdad0891255b2
auxiliawebsitedesign <preston@auxiliawebsitedesign.com>
2021-02-13 06:37:24 -0500

    Add models and policies also modify class name in policy

--- src/CheckoutServiceProvider.php
+++ src/CheckoutServiceProvider.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Tipoff\Checkout;
 
-use Tipoff\Checkout\Models\Checkout;
-use Tipoff\Checkout\Policies\CheckoutPolicy;
+use Tipoff\Checkout\Models\Cart;
+use Tipoff\Checkout\Models\CartItem;
+use Tipoff\Checkout\Models\Order;
+use Tipoff\Checkout\Policies\CartPolicy;
+use Tipoff\Checkout\Policies\CartItemPolicy;
+use Tipoff\Checkout\Policies\OrderPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -15,7 +19,9 @@ class CheckoutServiceProvider extends TipoffServiceProvider
     {
         $package
             ->hasPolicies([
-                Checkout::class => CheckoutPolicy::class,
+                Cart::class => CartPolicy::class,
+                CartItem::class => CartItemPolicy::class,
+                Order::class => OrderPolicy::class,
             ])
             ->name('checkout')
             ->hasConfigFile();
