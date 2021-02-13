367122310d9edc48cbc7924487e863575a8a0066
auxiliawebsitedesign <preston@auxiliawebsitedesign.com>
2021-02-13 05:21:17 -0500

    Update CheckoutServiceProvider.php

--- src/CheckoutServiceProvider.php
+++ src/CheckoutServiceProvider.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Tipoff\Checkout;
 
-use Tipoff\Checkout\Contracts\Models\CartInterface;
-use Tipoff\Checkout\Contracts\Models\CartItemInterface;
-use Tipoff\Checkout\Contracts\Models\OrderInterface;
-use Tipoff\Checkout\Models\Cart;
-use Tipoff\Checkout\Models\CartItem;
-use Tipoff\Checkout\Models\Order;
+use Tipoff\Checkout\Models\Checkout;
+use Tipoff\Checkout\Policies\CheckoutPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -18,14 +14,10 @@ class CheckoutServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
+            ->hasPolicies([
+                Checkout::class => CheckoutPolicy::class,
+            ])
             ->name('checkout')
             ->hasConfigFile();
-
-        $package
-            ->hasModelInterfaces([
-                CartInterface::class => Cart::class,
-                CartItemInterface::class => CartItem::class,
-                OrderInterface::class => Order::class,
-            ]);
     }
 }
