3f9d9ba4a6b8bc6eb7b177e592db5548473ce926
Drew Roberts <github@drewroberts.com>
2021-02-16 23:51:45 -0500

    Remove location from order factory #30

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -25,7 +25,6 @@ class OrderFactory extends Factory
     {
         return [
             'customer_id'  => randomOrCreate(app('customer')),
-            'location_id'  => randomOrCreate(app('location')),
             'amount'       => $this->faker->numberBetween(100, 40000),
             'total_taxes'    => $this->faker->numberBetween(10, 5000),
             'total_fees'   => $this->faker->numberBetween(10, 5000),
