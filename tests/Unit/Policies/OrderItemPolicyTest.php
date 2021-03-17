39c60d467bdd9a7eb799c1cdb4a0621092184d85
Drew Roberts <github@drewroberts.com>
2021-03-16 21:48:35 -0400

    Create order item policy test #2

---
+++ tests/Unit/Policies/OrderItemPolicyTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Checkout\Tests\Unit\Policies;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tipoff\Checkout\Models\OrderItem;
+use Tipoff\Checkout\Tests\TestCase;
+use Tipoff\Support\Contracts\Models\UserInterface;
+
+class OrderItemPolicyTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function view_any()
+    {
+        $user = self::createPermissionedUser('view order items', true);
+        $this->assertTrue($user->can('viewAny', OrderItem::class));
+
+        $user = self::createPermissionedUser('view order items', false);
+        $this->assertTrue($user->can('viewAny', OrderItem::class));
+    }
+}
