1128cdb80eebd54a5bc75d837974a8a8d2b2f5e1
Hunter Montell <64396917+huntermontell@users.noreply.github.com>
2021-03-22 18:51:39 -0500

    Update Cart Item Model Test

--- tests/Unit/Models/CartItemModelTest.php
+++ tests/Unit/Models/CartItemModelTest.php
@@ -5,44 +5,19 @@ declare(strict_types=1);
 namespace Tipoff\Checkout\Tests\Unit\Models;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Tipoff\Checkout\Models\CartItem;
-use Tipoff\Checkout\Tests\Support\Models\TestSellable;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tipoff\checkout\Models\CartItem;
 use Tipoff\Checkout\Tests\TestCase;
 
 class CartItemModelTest extends TestCase
 {
     use DatabaseTransactions;
+    use WithFaker;
 
     /** @test */
     public function create()
     {
-        TestSellable::createTable();
-        $sellable = TestSellable::factory()->create();
-
-        $model = CartItem::factory()->withSellable($sellable)->create();
+        $model = CartItem::factory()->create();
         $this->assertNotNull($model);
-    }
-
-    /** @test */
-    public function set_amount_each_updates_total()
-    {
-        TestSellable::createTable();
-        $sellable = TestSellable::factory()->create();
-
-        /** @var CartItem $cartItem */
-        $cartItem = CartItem::factory()->withSellable($sellable)->create([
-            'quantity' => 2,
-            'amount_each' => 1000,
-        ]);
-
-        $this->assertEquals(2000, $cartItem->getAmountTotal()->getOriginalAmount());
-        $this->assertEquals(0, $cartItem->getAmountTotal()->getDiscounts());
-        $this->assertEquals(2000, $cartItem->getAmountTotal()->getDiscountedAmount());
-
-        $cartItem->setAmountEach($cartItem->getAmountEach()->addDiscounts(250));
-
-        $this->assertEquals(2000, $cartItem->getAmountTotal()->getOriginalAmount());
-        $this->assertEquals(500, $cartItem->getAmountTotal()->getDiscounts());
-        $this->assertEquals(1500, $cartItem->getAmountTotal()->getDiscountedAmount());
     }
 }
